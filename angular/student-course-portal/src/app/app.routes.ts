818069b384efc456673cae1df531c21b1f9e25ce
bhavanadm147 <4bd23cs032@bietdvg.edu>
2026-07-24 21:48:37 +0530

    Complete Angular Hands-On 4

--- angular/student-course-portal/src/app/app.routes.ts
+++ angular/student-course-portal/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { CourseList } from './pages/course-list/course-list';
 import { StudentProfile } from './pages/student-profile/student-profile';
+import { EnrollmentForm } from './pages/enrollment-form/enrollment-form';
 
 export const routes: Routes = [
   {
@@ -15,5 +16,9 @@ export const routes: Routes = [
   {
     path: 'profile',
     component: StudentProfile
+  },
+  {
+    path: 'enroll',
+    component: EnrollmentForm
   }
 ];
